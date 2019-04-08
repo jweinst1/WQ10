498a98dc31ab2f07b70ba05c96ffbb7adb726bc1
Joshua Weinstein <jweinstein@splunk.com>
2019-04-08 15:36:51 -0700

    begin hashing code

--- main.c
+++ main.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "wq10_types.h"
 
+static struct wq10_info WQ_10_OPTIONS;
+
+static void clear_wq10_options(void)
+{
+	WQ_10_OPTIONS.input = NULL;
+	WQ_10_OPTIONS.input_len = 0;
+	WQ_10_OPTIONS.output_path = NULL;
+}
+
+static int get_wq10_options(const char** arguments)
+{
+	clear_wq10_options();
+	while(*arguments != NULL)
+	{
+		if(strcmp(WQ10_OUTPUT_OPTION, *arguments) == 0) {
+			arguments++;
+			WQ_10_OPTIONS.output_path = *arguments;
+			arguments++;
+
+		} else if(strcmp(WQ10_INPUT_OPTION, *arguments) == 0) {
+			arguments++;
+			WQ_10_OPTIONS.input = *arguments;
+			WQ_10_OPTIONS.input_len = strlen(WQ_10_OPTIONS.input);
+			arguments++;
+		} else {
+			fprintf(stderr, "Error: Found unknown option '%s'.\n", *arguments);
+			return 0;
+		}
+	}
+	if(WQ_10_OPTIONS.input == NULL) {
+		fprintf(stderr, "Error: Option '%s' not found.\n", WQ10_INPUT_OPTION);
+		return 0;
+	} else if(WQ_10_OPTIONS.output_path == NULL) {
+		fprintf(stderr, "Error: Option '%s' not found.\n", WQ10_OUTPUT_OPTION);
+		return 0;
+	}
+	return 1;
+}
 
 
 
--- wq10_types.h
+++ wq10_types.h
@@ -17,8 +17,12 @@ struct rgb_t {
 	unsigned char blue;
 	unsigned char green;
 };
-
-
+/**
+ * This struct represents the 10 x 10 grid of colored squares.
+ * Once the hash completes, these colors will be expanded into the actual
+ * pixels in the output_buf. Since all the pixels in the same square
+ * are the same color, we only need one rgb_t to keep track of the color.
+ */
 struct image_grid {
 	struct rgb_t hash_blocks[100];
 };
@@ -26,6 +30,11 @@ struct image_grid {
 struct output_buf {
 	unsigned char data[WQ10_OUTPUT_BUF_SIZE];
 };
+
+/*option macro forms*/
+
+#define WQ10_OUTPUT_OPTION "--output"
+#define WQ10_INPUT_OPTION "--input"
 
 
 #endif // WQ10_TYPES_H
